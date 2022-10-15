fc09a6d47c29ca2db6c9ce3af563647def2deb9a
randilu <randilu.s@gmail.com>
2022-10-15 13:15:44 +0530

    Host the app

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes/v1');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 // v1 api routes
 app.use('/v1', routes);
