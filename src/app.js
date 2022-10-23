0d8701c19dc084a3a560042f2cbdad2ab3cae05d
randilu <randilu.s@gmail.com>
2022-10-23 23:30:04 +0530

    Update api path

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const routes = require('./routes/v1');
+const routes = require('./routes/api/v2');
 
 const app = express();
 
 app.use(express.json());
 
 // v1 api routes
-app.use('/v1', routes);
+app.use('/api/v2', routes);
 
 module.exports = app;
