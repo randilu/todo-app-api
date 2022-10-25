a088980b9a1f9098ac95980aadb1a1255e5fdfd4
randilu <randilu.s@gmail.com>
2022-10-26 00:40:52 +0530

    updated v1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-if (process.env.NODE_ENV !== 'production') {
-  // eslint-disable-next-line global-require
-  require('dotenv').config();
-}
 const app = require('./app');
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.listen(PORT, () => {
   console.log(`Listening to port ${PORT}`);
