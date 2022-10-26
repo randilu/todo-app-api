441a691518486f8b4040a114d25395855ef3aa84
randilu <randilu.s@gmail.com>
2022-10-26 14:15:18 +0530

    Update mocks

--- src/__mocks__/todo.js
+++ src/__mocks__/todo.js
@@ -1,18 +1,21 @@
 const mockTodos = [
   {
     id: '1',
-    item: 'Make the cake',
+    title: 'Make the cake',
+    description: "First you'll have to choose a recipe. You can keep it simple or you can choose a slightly showier recipe..",
     status: 'completed',
   },
   {
     id: '2',
-    item: 'Bake the cake',
+    title: 'Bake the cake',
+    description: "Divide the batter evenly between the baking pans. Bake your cake according to your recipe's directions.",
     status: 'completed',
   },
   {
     id: '3',
-    item: 'Eat the cake',
-    status: 'completed',
+    title: 'Serve the cake',
+    description: "Most cakes should be cut with a chef's knife. Use a long serrated knife for cakes with fresh fruit filling",
+    status: 'in-progress',
   },
 ];
 
